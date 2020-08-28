92852c20e16ffe4c438206c7f6f05d2f920b2bf6
Phil <phil@evolvedigital.co.uk>
2020-08-28 23:06:24 +0100

    Add api fetch and build submenu nav list

--- app.js
+++ app.js
@@ -2,13 +2,15 @@
 const navSlide = () => {
     const burger = document.querySelector('.burger');
     const nav = document.querySelector('.nav-links');
+    const subNav = document.querySelector('.sub-menu');
     // get all nav links
     const navLinks = document.querySelectorAll('.nav-links li');
+    const subNavLinks = document.querySelectorAll('.sub-menu li');
 
     // Toggle nav
     burger.addEventListener('click', function() {
         nav.classList.toggle('nav-active');
-        
+
         // Animate links
         navLinks.forEach((link, index) => {
             // console.log(index);
@@ -25,9 +27,40 @@ const navSlide = () => {
 
 }
 
+/* Use fetch api to get list of projects hosted elsewhere */
+const projectsList = () => {
+
+    // Get json data from url
+    const proxy = 'https://cors-anywhere.herokuapp.com/'; // use proxy to fix cors on localhost
+    const url = `${proxy}https://utopianfool.github.io/link-api/projects.json`;
+
+
+    fetch(url) 
+        .then(response => {
+            return response.json();
+        })
+        .then(data => {
+            // console.log(data);
+            appendData(data);
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    function appendData(data) {
+        var subMenu = document.querySelector('.sub-menu');
+        for(var i = 0; i < data.length; i++) {
+            var listItem = document.createElement('li');
+            listItem.innerHTML = '<a title="' + data[i].description + '" href="' + data[i].link_url + '">' + data[i].link_text + '</a>';
+            subMenu.appendChild(listItem);
+        }
+    } // https://howtocreateapps.com/fetch-and-display-json-html-javascript/
+
+}
+
 // keep it tidy by invoking smaller functions inside of app function
 const app = () => {
     navSlide();
+    projectsList();
 }
 
 app();
